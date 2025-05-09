06ab422da239f4392adf413261707ad29a76571f
jinhkk <kwlsgur00@naver.com>
2025-05-09 23:19:17 +0900

    반응형 하려다가 잠시 휴식식

--- 000/js/main.js
+++ 000/js/main.js
@@ -1,29 +1,29 @@
-$(function(){
-    $('.search').click(function(){
+$(function () {
+    $('.search').click(function () {
         $('.searchf').slideToggle()
     })
-    $('.hbot > ul > li').mouseenter(function(){
+    $('.hbot > ul > li').mouseenter(function () {
         $('header').stop().animate({
             height: '484px'
-        },400)
+        }, 400)
     })
-    $('.hbot > ul > li').mouseleave(function(){
+    $('.hbot > ul > li').mouseleave(function () {
         $('header').stop().animate({
             height: '168px'
-        },400)
+        }, 400)
     })
-    
+
 })
 
-let yes=document.querySelector(".yes")
-let no=document.querySelector(".no")
-let pbg=document.querySelector(".popup_bg")
+let yes = document.querySelector(".yes")
+let no = document.querySelector(".no")
+let pbg = document.querySelector(".popup_bg")
 
-    yes.addEventListener("click",function(){
-        pbg.style.display="none";
-    })
+yes.addEventListener("click", function () {
+    pbg.style.display = "none";
+})
 
-    no.addEventListener("click",function(){
-        alert('만 19세 미만은 사이트 이용이 불가능 합니다.')
-    })
+no.addEventListener("click", function () {
+    alert('만 19세 미만은 사이트 이용이 불가능 합니다.')
+})
 
