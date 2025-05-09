88f30be02626742afd6c02d0d44408604ca252ff
jinhkk <kwlsgur00@naver.com>
2025-05-09 16:07:24 +0900

    hbot 잘 안맞음

--- 000/js/main.js
+++ 000/js/main.js
@@ -14,3 +14,16 @@ $(function(){
     })
     
 })
+
+let yes=document.querySelector(".yes")
+let no=document.querySelector(".no")
+let pbg=document.querySelector(".popup_bg")
+
+    yes.addEventListener("click",function(){
+        pbg.style.display="none";
+    })
+
+    no.addEventListener("click",function(){
+        alert('만 19세 미만은 사이트 이용이 불가능 합니다.')
+    })
+
